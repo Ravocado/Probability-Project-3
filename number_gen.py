96b89c79d4259ae7561d3bf8b8239ce85cf403a4
Ravi Jayaraman <spy7sw@virginia.edu>
2022-04-27 18:21:13 -0400

    Started Project

---
+++ number_gen.py
@@ -0,0 +1,27 @@
+class numberGen:
+
+    def __init__(self):
+        #Defines parameters of random number generator
+        self.x = 1000
+        self.A = 24693
+        self.C = 3967
+        self.K = 2**18
+
+    #Gets the next random number in the sequence
+    def getNext(self):
+        self.x = (self.x * self.A + self.C) % self.K
+        u = self.x/self.K
+        return u
+
+    #Gets a specified random number in the sequence at position c
+    def getSpec(self, i):
+        x0 = 1000
+        for j in range(i):
+            x0 = (x0 * self.A + self.C) % self.K
+            
+        u = x0/self.K
+        return u
+
+    
+
+
---
+++ test.py
@@ -0,0 +1,6 @@
+from number_gen import numberGen
+
+ng = numberGen()
+print(ng.getSpec(51))
+print(ng.getSpec(52))
+print(ng.getSpec(53))
